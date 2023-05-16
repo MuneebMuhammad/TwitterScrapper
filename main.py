1bfe8ea436e1fc2da3d2773474fb3ee46d2c6488
Muneeb Muhammad <muneebkattak57@gmail.com>
2023-05-16 17:55:39 +0500

    errors in hashtag resolved

--- main.py
+++ main.py
@@ -10,6 +10,63 @@ import pandas as pd
 driver = webdriver.Chrome()
 # opens a userprofile and gets the first 'n' tweets
 
+def get_n_tweets_from_current_page(n):
+    user_tags = []
+    time_stamps = []
+    tweets = []
+    no_replies_list = []
+    no_re_tweet_list = []
+    no_like_list = []
+
+    last_height = 10
+    # print(len(tweet_container))
+    while True:
+        tweet_container = driver.find_elements(by="xpath",
+                                               value="//article[@data-testid='tweet']")  # getting tweet container
+        try:
+            for tweet in tweet_container:
+                user_tag = driver.find_element(by="xpath",
+                                               value="//div[@data-testid='User-Name']//*//*//*//*//*//*//span")
+                time_stamp = driver.find_element(by="xpath", value="//time")  # getting time stamp
+                tweet = driver.find_element(by="xpath", value="//div[@data-testid='tweetText']//span")  # getting tweet
+                no_replies = driver.find_element(by="xpath",
+                                                 value="//div[@data-testid='reply']//div[position()=2]//span//span//span")  # getting reply
+                no_re_tweet = driver.find_element(by="xpath",
+                                                  value="//div[@data-testid='retweet']//div[position()=2]//span//span//span")  # getting re-tweet
+                no_like = driver.find_element(by="xpath",
+                                              value="//div[@data-testid='like']//div[position()=2]//span//span//span")  # getting like
+
+                if len(tweet.text) > 1 and tweet.text not in tweets:
+                    user_tags.append(user_tag.text)
+                    time_stamps.append(time_stamp.get_attribute("datetime"))
+                    tweets.append(tweet.text)
+                    no_replies_list.append(no_replies.text)
+                    no_re_tweet_list.append(no_re_tweet.text)
+                    no_like_list.append(no_like.text)
+        except:
+            print("error")
+        # driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        driver.execute_script(f"window.scrollTo(0, {last_height});")
+        time.sleep(0.1)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height <= last_height:
+            break
+        Tweets2 = list(set(tweets))
+        last_height += 300
+        if len(Tweets2) > 3:
+            break
+
+    user_tags = list(dict.fromkeys(user_tags))
+    time_stamps = list(dict.fromkeys(time_stamps))
+    tweets = list(dict.fromkeys(tweets))
+    no_replies_list = list(dict.fromkeys(no_replies_list))
+    no_re_tweet_list = list(dict.fromkeys(no_re_tweet_list))
+    no_like_list = list(dict.fromkeys(no_like_list))
+
+    df = pd.DataFrame(zip(user_tags, time_stamps, tweets, no_replies_list, no_re_tweet_list, no_like_list),
+                      columns=["User Tags", "Time Stamps", "Tweets", "No. Replies", "No. Retweets", "No. likes"])
+    return df
+
 def login(username="MuneebWale80142", password="muneebwalee"):
     driver.get("https://twitter.com/login")
     time.sleep(30)  # wait for 4 seconds
@@ -33,11 +90,12 @@ def get_n_tweets(n, username):
 
     wait = WebDriverWait(driver, 10)
 
-    time.sleep(20)
+    time.sleep(30)
 
     last_height = 10
     tweets = []
     while len(tweets) < n:
+        print("Tweets:", len(tweets))
         driver.execute_script(f"window.scrollTo(0, {last_height});")
         time.sleep(0.1)
         new_height = driver.execute_script("return document.body.scrollHeight")
@@ -141,34 +199,41 @@ def getHashtagTweets(hashtag="elonmusk"):
     no_re_tweet_list = []
     no_like_list = []
 
-    tweet_container = driver.find_elements(by="xpath",
-                                           value="//article[@data-testid='tweet']")  # getting tweet container
+    last_height = 10
     # print(len(tweet_container))
     while True:
-
-        for tweet in tweet_container:
-            user_tag = driver.find_element(by="xpath", value="//div[@data-testid='User-Name']//*//*//*//*//*//*//span")
-            time_stamp = driver.find_element(by="xpath", value="//time")  # getting time stamp
-            tweet = driver.find_element(by="xpath", value="//div[@data-testid='tweetText']//span")  # getting tweet
-            no_replies = driver.find_element(by="xpath",
-                                             value="//div[@data-testid='reply']//div[position()=2]//span//span//span")  # getting reply
-            no_re_tweet = driver.find_element(by="xpath",
-                                              value="//div[@data-testid='retweet']//div[position()=2]//span//span//span")  # getting re-tweet
-            no_like = driver.find_element(by="xpath",
-                                          value="//div[@data-testid='like']//div[position()=2]//span//span//span")  # getting like
-
-            user_tags.append(user_tag.text)
-            time_stamps.append(time_stamp.get_attribute("datetime"))
-            tweets.append(tweet.text)
-            no_replies_list.append(no_replies.text)
-            no_re_tweet_list.append(no_re_tweet.text)
-            no_like_list.append(no_like.text)
-
-        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-        time.sleep(2)
+        tweet_container = driver.find_elements(by="xpath",
+                                               value="//article[@data-testid='tweet']")  # getting tweet container
+        try:
+            for tweet in tweet_container:
+                user_tag = driver.find_element(by="xpath", value="//div[@data-testid='User-Name']//*//*//*//*//*//*//span")
+                time_stamp = driver.find_element(by="xpath", value="//time")  # getting time stamp
+                t = driver.find_element(by="xpath", value="//div[@data-testid='tweetText']")  # getting tweet
+                no_replies = driver.find_element(by="xpath",
+                                                 value="//div[@data-testid='reply']//div[position()=2]//span//span//span")  # getting reply
+                no_re_tweet = driver.find_element(by="xpath",
+                                                  value="//div[@data-testid='retweet']//div[position()=2]//span//span//span")  # getting re-tweet
+                no_like = driver.find_element(by="xpath",
+                                              value="//div[@data-testid='like']//div[position()=2]//span//span//span")  # getting like
+                print("tweet:", t.text)
+                if len(tweet.text) > 1 and t.text not in tweets:
+                    user_tags.append(user_tag.text)
+                    time_stamps.append(time_stamp.get_attribute("datetime"))
+                    tweets.append(t.text)
+                    no_replies_list.append(no_replies.text)
+                    no_re_tweet_list.append(no_re_tweet.text)
+                    no_like_list.append(no_like.text)
+        except:
+            print("error")
+        # driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        driver.execute_script(f"window.scrollTo(0, {last_height});")
+        time.sleep(0.1)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height <= last_height:
+            break
         Tweets2 = list(set(tweets))
-
-        if len(Tweets2) > 30:
+        last_height += 300
+        if len(Tweets2) > 3:
             break
 
     user_tags = list(dict.fromkeys(user_tags))
@@ -182,15 +247,17 @@ def getHashtagTweets(hashtag="elonmusk"):
     df = pd.DataFrame(zip(user_tags, time_stamps, tweets, no_replies_list, no_re_tweet_list, no_like_list),
                       columns=["User Tags", "Time Stamps", "Tweets", "No. Replies", "No. Retweets", "No. likes"])
 
-    df.head()
+    print(df.head())
 
-    df.to_excel(r"D:\\tweets.xlsx", index=False)
+    # df.t(f"hashtag:{hashtag}.xlsx", index=False)
+    df.to_csv(f"hashtag:{hashtag}.xlsx", index=False, sep=',')
 
+# def hashtag_tweets()
 
-# login()
-# getHashtagTweets()
+login()
+getHashtagTweets()
 # get_top_trends()
-get_n_tweets(100, 'elonmusk')
+# get_n_tweets(100, 'elonmusk')
 # get_explore_tweets(10)
 # get_tweet_from_URL("https://twitter.com/Exulansista/status/1654540589730521088")
 
